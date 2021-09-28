b004ad8ca7b055e50f7ba88500482c44483be241
Ruchi Tetwal <ruchi2452002@gmail.com>
2021-09-28 20:36:07 +0530

    4th commit

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -41,20 +43,24 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render("secrets");
+            }
+        });
     });
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("secrets");
-        }
-    })
-})
+});
 
 app.post("/login", function(req,res){
     const username = req.body.username;
@@ -66,9 +72,15 @@ app.post("/login", function(req,res){
         }
         else{
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
+                //if(foundUser.password === password){
+                  //  res.render("secrets");
+               // }
+
+               bcrypt.compare(password, foundUser.password, function(err, result){
+                   if(result === true){
+                       res.render("secrets");
+                   }
+               })
             }
         }
     });
